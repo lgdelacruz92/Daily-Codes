5ef65beb76681349318459deaa970e631829aecb
Lester Dela Cruz <lgdelacruz92@gmail.com>
2021-03-18 14:25:56 -0700

    so far so good

--- scripts/codecolorizer/client/java/convert-to-content-resource.py
+++ scripts/codecolorizer/client/java/convert-to-content-resource.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-with open(sys.argv[1],'r') as test_convert:
+with open('java_pygments.html','r') as test_convert:
     s = test_convert.read()
     s2 = json.dumps(s)
     print(s2)
